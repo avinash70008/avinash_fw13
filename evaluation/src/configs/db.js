95d3fb1d2f1ddeab556820b36a08729c8a2d93db
Avinash Kumar <avinash.kumar70008@gmail.com>
2022-02-21 18:46:23 -0800

    hi

--- evaluation/src/configs/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-module.exports = () => {
-    return mongoose.connect("mongodb+srv://avinashtest:test_123@cluster0.w0khx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-};
